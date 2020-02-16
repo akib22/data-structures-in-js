ea4b6952657938669c920e52ffebe08075f5aa17
akib22 <adnanakib75@gmail.com>
2020-02-16 19:55:30 +0600

    Add push method in singly-linked-list

--- linked-lists/singly-linked-lists/singlyLinkedLists.js
+++ linked-lists/singly-linked-lists/singlyLinkedLists.js
@@ -11,6 +11,21 @@ class SinglyLinkedLIst {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const node = new Node(val);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+
+    this.length++;
+    return node;
+  }
 }
 
 module.exports = SinglyLinkedLIst;
--- linked-lists/test/singlyLinkedList.test.js
+++ linked-lists/test/singlyLinkedList.test.js
@@ -4,16 +4,77 @@ describe('Singly Linked List', () => {
   describe('Instantiating SinglyLinkedList', () => {
     const list = new SinglyLinkedLIst();
 
-    it('should head to be null', () => {
-      expect(list.head).toBe(null);
+    it('should list\'s head to be null', () => {
+      expect(list.head).toBeNull();
     });
 
-    it('should tail to be null', () => {
-      expect(list.head).toBe(null);
+    it('should list\'s tail to be null', () => {
+      expect(list.head).toBeNull();
     });
 
-    it('should length equal to 0', () => {
+    it('should list\'s length equal to 0', () => {
       expect(list.length).toEqual(0);
+    });
+  });
+
+  describe('push', () => {
+    it('should return a node with value of passing params', () => {
+      const list = new SinglyLinkedLIst();
+      expect(list.push('22').value).toEqual('22');
+    });
+
+    describe('for only one (first time) call', () => {
+      const list = new SinglyLinkedLIst();
+      list.push(1);
+
+      it('should list\'s head value to be 1 and list\'s head next to be null ', () => {
+        expect(list.head.value).toEqual(1);
+        expect(list.head.next).toBeNull();
+      });
+
+      it('should list\'s tail value to be 1 and list\'s tail next to be null ', () => {
+        expect(list.tail.value).toEqual(1);
+        expect(list.tail.next).toBeNull();
+      });
+
+      it('should head and tail to be same object.', () => {
+        expect(list.head).toBe(list.tail);
+      });
+
+      it('should list\'s length equal to 1', () => {
+        expect(list.length).toEqual(1);
+      });
+    });
+
+    describe('for more than one call', () => {
+      const list = new SinglyLinkedLIst();
+      const callCounter = 3;
+      
+      for(let i = 1; i <= callCounter; i++) {
+        list.push(i);
+      }
+
+      it('should not head and tail to be same object.', () => {
+        expect(list.head).not.toBe(list.tail);
+      });
+
+      it('should list\'s head value equal to 1 and next not to be null ', () => {
+        expect(list.head.value).toEqual(1);
+        expect(list.head.next).not.toBeNull();
+      });
+
+      it('should list\'s second node value to be 2', () => {
+        expect(list.head.next.value).toEqual(2);
+      });
+
+      it(`should list\'s tail value equal to ${callCounter}(callCounter) and next to be null`, () => {
+        expect(list.tail.value).toEqual(callCounter);
+        expect(list.tail.next).toBeNull();
+      });
+
+      it(`should list\'s length equal to ${callCounter}(callCounter)`, () => {
+        expect(list.length).toEqual(callCounter);
+      });
     });
   });
 });

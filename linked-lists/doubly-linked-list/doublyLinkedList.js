9e0c1fd44f4c9cc17efee43d60399ee75fd38de7
akib22 <adnanakib75@gmail.com>
2020-05-20 21:18:56 +0600

    Init doubly-linked-list

---
+++ linked-lists/doubly-linked-list/doublyLinkedList.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = null;
+  }
+}
+
+module.exports = DoublyLinkedList;
---
+++ linked-lists/test/doublyLinkedList.test.js
@@ -0,0 +1,5 @@
+const DoublyLinkedLIst = require('../singly-linked-lists/doublyLinkedLists');
+
+describe('Doubly Linked List', () => {
+
+});

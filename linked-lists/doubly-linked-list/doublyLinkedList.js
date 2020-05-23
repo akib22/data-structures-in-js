9db97892c3099c67e199bbf3b3b66ccee227ae88
akib22 <adnanakib75@gmail.com>
2020-05-23 16:49:09 +0600

    Add push method in doubly-linked-list

--- linked-lists/doubly-linked-list/doublyLinkedList.js
+++ linked-lists/doubly-linked-list/doublyLinkedList.js
@@ -10,7 +10,28 @@ class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
-    this.length = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    if (value === undefined) {
+      return undefined;
+    }
+
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = this.tail.next;
+    }
+
+    this.length += 1;
+
+    return newNode;
   }
 }
 
--- linked-lists/test/doublyLinkedList.test.js
+++ linked-lists/test/doublyLinkedList.test.js
@@ -1,5 +1,51 @@
-const DoublyLinkedLIst = require('../singly-linked-lists/doublyLinkedLists');
+const DoublyLinkedLIst = require('../doubly-linked-list/doublyLinkedList');
 
 describe('Doubly Linked List', () => {
+  describe('Instantiating DoublyLinkedLIst', () => {
+    const list = new DoublyLinkedLIst();
 
+    it("should list's head and tail to be null", () => {
+      expect(list.head).toBeNull();
+      expect(list.tail).toBeNull();
+    });
+
+    it("should list's length equal to 0", () => {
+      expect(list.length).toEqual(0);
+    });
+  });
+
+  describe('push', () => {
+    const list = new DoublyLinkedLIst();
+
+    it('should return undefined when call with undefined value', () => {
+      expect(list.push()).toBeUndefined();
+    });
+
+    it('should increase list length by 1', () => {
+      list.push(1);
+      expect(list.length).toBe(1);
+    });
+
+    describe('when list is empty', () => {
+      const list = new DoublyLinkedLIst();
+      list.push(1);
+
+      it("should list's head value to be 1 and list's head next to be null", () => {
+        expect(list.head.value).toEqual(1);
+        expect(list.head.next).toBeNull();
+      });
+
+      it("should list's tail value to be 1 and list's tail next to be null", () => {
+        expect(list.tail.value).toEqual(1);
+        expect(list.tail.next).toBeNull();
+      });
+    });
+
+    describe('when list list is not empty', () => {
+      it('should add new node at the end as tail.', () => {
+        list.push(2);
+        expect(list.tail.value).toBe(2);
+      });
+    });
+  });
 });
